6c1f561ffe9981aa7671427ff7c3790cf783ac0b
Diederikvanlangen <diederikvanlangen@hotmail.nl>
2015-10-24 17:18:00 +0200

    .

--- src/com/yadeses/duitsKruisWordPuzzel/main/Frame.java
+++ src/com/yadeses/duitsKruisWordPuzzel/main/Frame.java
@@ -1,8 +1,6 @@
 package com.yadeses.duitsKruisWordPuzzel.main;
 
 import java.awt.*;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridBagLayout;
@@ -10,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -26,7 +25,7 @@ public class Frame extends JFrame implements ActionListener {
 
 	public Frame() {
 		super("Puzzle");
-		
+		String Schlüsselwort = "Schweiz";
 		List<Map<String, String>> Words = new ArrayList<Map<String, String>>(5);
 		Words = App.readXML("test.xml");
 
@@ -34,31 +33,46 @@ public class Frame extends JFrame implements ActionListener {
 		GridBagConstraints c = new GridBagConstraints();
 		JMenuBar bar = new JMenuBar();
 
-		
+		JButton but1 = new JButton("Erklärung");
+		but1.addActionListener(this);
 		JButton but2 = new JButton("Kontrollieren");
 		JTextField text = new JTextField("Umschreibungen");
 		text.setEditable(false);
 		Document doc = text.getDocument();
 		((AbstractDocument) doc).setDocumentFilter(new SizeDocumentFilter());
-		
-		bar.add(but2);		
+	
+		bar.add(but2);
+		bar.add(but1);
 		c.ipadx = 20;
 		c.ipady = 8;
 		JTextField cells[][] = new JTextField[20][20];
-		for (int i = 0; i < 12; i++) {
+		for (int i = 0; i < 13; i++) {
 			c.gridx = 0;
-			c.gridy = 20+i;
-			String getal = "" + (i+1);
+			c.gridy = 13+i;
+			String getal;
+			if(i<12){
+				
+			
+			getal = "" + (i+1);}
+			else
+				getal = "Schlüsselwort";
 			JLabel o = new JLabel(getal);			
 			add(o,c);
 			for (int j = 0; j < 20; j++) {
 				c.gridx = j+1;
 				cells[i][j] = new JTextField();
-				add(cells[i][j], c);
+				cells[i][j].setOpaque( true );
+				/*JLabel label = new JLabel( new ImageIcon("C:\\Users\\johan\\workspace\\Diederik\\src\\ProjektSchweiz\\1.gif") );				
+				label.add( cells[i][j],c);
 				((AbstractDocument) ((JTextField) cells[i][j]).getDocument())
 						.setDocumentFilter(new SizeDocumentFilter());
+				add(label, c);*/
+				add(cells[i][j],c);
+				
+				
 			}
-		}
+		}		
+		
 		JLabel Umschreibungen = new JLabel("Umschreibungen");
 		c.gridy=0;
 		c.gridx=10;
@@ -74,7 +88,9 @@ public class Frame extends JFrame implements ActionListener {
 			c.ipady = 12;
 			add(omschrijving,c);
 		}
-		setSize(800, 850);
+		
+		
+		setSize(900, 900);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setJMenuBar(bar);
 		setVisible(true);
@@ -92,7 +108,17 @@ public class Frame extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+		if(e.getActionCommand()=="Erklärung"){
+		JOptionPane.showMessageDialog(null,""
+				+ "In diesem Spiel sollst du probieren das Schlüsselwort \n"
+				+ "von logisch Nachdenken zu raten. Dafür musst du erst \n"
+				+ "schwanzig Fragen beantworten, die sich handeln um die Schweiz.\n"
+				+ "Trag in jedes Feld einen Letter ein. Wenn in mehreren Felder\n"
+				+ "die selbe Zahlen sind, sollen in diese Felder auch die\n"
+				+ "selbe Letter gesezt werden. Wenn du alle Zahlen des \n"
+				+ "Schlüsselworts weißt, kannst du es erfüllen und hast du\n"
+				+ "das Spiel gewonnen!","Spielerklärung", JOptionPane.PLAIN_MESSAGE);
+	}
 
 	}
 }
